abb9c04400d2f73dd1eaefc32148fae4dde195c7
Guillaume Falourd <guillaume.falourd@zup.com.br>
2021-05-07 18:52:02 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ if [ -d "$2" ]; then
     echo "Cloned $2 repository successfully."
     echo "Access the repository content using \"cd $2\"." 
 else
-    echo "Error: Couldn't clone $2 repository. Check the PAT scope."
+    echo "Error: Couldn't clone $2 repository. Check the inputs or the PAT scope."
     exit 1
 fi
